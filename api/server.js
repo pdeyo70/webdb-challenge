8a302aa4f2595d2b08a80544f24d16bfa7f7ca94
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-30 10:28:17 -0500

    got a get query working that returns all the data requested; it is not in the nested format specified by MVP, but it is progress

--- api/server.js
+++ api/server.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/challenge.db3',
+    },
+    useNullAsDefault: true, // needed for sqlite
+};
+const db = knex(knexConfig);
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-const knex = require('knex');
 
-const db = require('../data/db.js');
 
-server.get('/api/projects',  (req, res) => {
-    const projects =  db('projects');
-    if (projects) {
+
+
+server.get('/api/projects', async (req, res) => {
+    // get the roles from the database
+    try {
+        const projects = await 
+            db('projects')
+                .join('actions', {'actions.project_id': 'projects.id'});
+        console.log(projects); // all the records from the table
         res.status(200).json(projects);
-    } else {
-        error => {
-            res.status(500).json(error);
-        }
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
