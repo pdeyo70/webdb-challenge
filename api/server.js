11b42f2b6fa46738ffd6e77185ab21c0d194ceb6
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-30 18:00:06 -0500

    query still works; got posts to the projects and actions table in place and working; unsure of how to fix query to get it to display the way MVP is wanting.

--- api/server.js
+++ api/server.js
@@ -22,14 +22,46 @@ server.use(express.json());
 server.get('/api/projects', async (req, res) => {
     // get the roles from the database
     try {
-        const projects = await 
+        const projects = await
             db('projects')
-                .join('actions', {'actions.project_id': 'projects.id'});
+            .join('actions', {'actions.project_id': 'projects.id'});
         console.log(projects); // all the records from the table
         res.status(200).json(projects);
     } catch (error) {
         res.status(500).json(error);
     }
 });
+
+server.post('/api/projects', async (req, res) => {
+    try {
+        const [id] = await db('projects').insert(req.body);
+
+        const project = await db('projects')
+            .where({ id })
+            .first();
+
+        res.status(201).json(project);
+    } catch (error) {
+        const message = errors[error.errno] || 'We ran into an error';
+        res.status(500).json({ message, error });
+    }
+});
+
+server.post('/api/actions', async (req, res) => {
+    try {
+        const [id] = await db('actions').insert(req.body);
+
+        const action = await db('actions')
+            .where({ id })
+            .first();
+
+        res.status(201).json(action);
+    } catch (error) {
+        const message = errors[error.errno] || 'We ran into an error';
+        res.status(500).json({ message, error });
+    }
+});
+
+
 
 module.exports = server;
